920434a462b234d6735f96cc226636c30d73611c
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2015-04-06 17:24:47 +0800

    My first Go code!

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "os"
+
+// welcome to the go world, pat!
+
+/*
+Another way of writing comments in Go
+*/
+
+func main(){
+  fmt.Println("Hello, my name is Pat!")
+  os.Exit(0)
+}
